b7cf9b402265009c2dc6626b17cfbeed0db76840
Russ <russ.dermody@gmail.com>
2019-03-13 15:11:04 -0700

    fiddling with handling player input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+//playerGuess will hold all player inputs.
+var playerGuess = "";
+//badGuesses will hold exactly those
+var badGuesses = "";
+//var to contain number of lives
+var livesLeft = 15;
 // Array to hold all guessable strings
 var guessUs = ["Covenant", "Prometheus", "Aliens"];
 // Variable will hold current word user is guessing.
@@ -18,6 +24,24 @@ for (var i = 0; i < guessMe.length; i++) {
 //document.getElementById("guess-me-test").innerHTML = guessMe;
 document.getElementById("underscores").innerHTML = guessLine.join(" ");
 
+//Handles player input
+//keypress used instead of keydown/up, as it is better for this application
+window.addEventListener("keypress", processInput); 
+//function called in line above, stores individiual inputs
+function processInput(event) {
+    playerGuess = String.fromCharCode(event.charCode);
+    console.log(playerGuess);
+}
+//Loop to check if guess is good or bad
+for (var j = 0; j < guessMe.length; j++) {
+    if (guessMe[j] === playerGuess) {
+        guessLine[j] === playerGuess;
+    }
+    else {
+        badGuesses += playerGuess;
+        livesLeft--;
+    }
+}
 
 //Game Loop
 //while (remainingLetters > 0) {}
@@ -26,3 +50,5 @@ document.getElementById("underscores").innerHTML = guessLine.join(" ");
 console.log(guessMe)
 console.log(guessLine)
 console.log(remainingLetters)
+console.log(playerGuess)
+console.log(livesLeft)
